e9ee45828fc4c7911f024f6b8210189001ed292d
Christopher Ndung'u <dev.kiratu@gmail.com>
2023-11-29 12:50:00 +0300

    build promises before passing them to resolver

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -2,7 +2,9 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
+  const userPromise = signUpUser(firstName, lastName);
+  const photoPromise = uploadPhoto(fileName);
+  return Promise.allSettled([userPromise, photoPromise])
     .then((values) => {
       const result = [];
       values.forEach((v) => {
