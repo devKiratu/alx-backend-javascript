baf7d7937f9a30a17fc85fdba782bf39bc20c7e3
Christopher Ndung'u <dev.kiratu@gmail.com>
2023-12-05 09:07:52 +0300

    handle out of range error

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -1,7 +1,11 @@
 export default function createInt8TypedArray(length, position, value) {
-  const buffer = new ArrayBuffer(length);
-  const dataView = new DataView(buffer);
-  dataView.setInt8(position, value);
+  try {
+    const buffer = new ArrayBuffer(length);
+    const dataView = new DataView(buffer);
+    dataView.setInt8(position, value);
 
-  return dataView;
+    return dataView;
+  } catch (e) {
+    throw new RangeError('Position outside range');
+  }
 }
