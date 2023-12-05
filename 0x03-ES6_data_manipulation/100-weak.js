4b691ca70d8f31fbfb4d4e925b0d7b748eac1ddb
Christopher Ndung'u <dev.kiratu@gmail.com>
2023-12-05 11:13:23 +0300

    refactor: clean up queryApi function

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -1,11 +1,11 @@
 export const weakMap = new WeakMap();
 
 export function queryAPI(endpoint) {
-  const number = weakMap.get(endpoint);
+  const number = weakMap.get(endpoint) || 0;
 
   if (number >= 5) {
     throw new Error('Endpoint load is high');
   }
 
-  weakMap.set(endpoint, number ? number + 1 : 1);
+  weakMap.set(endpoint, number + 1);
 }
