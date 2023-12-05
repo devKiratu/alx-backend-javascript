1e7f32870b1245688a24da923187cb94bf8f7fec
Christopher Ndung'u <dev.kiratu@gmail.com>
2023-12-05 10:06:05 +0300

    ensure set values are string before calling the startsWith method on them

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -2,7 +2,7 @@ export default function cleanSet(set, startString) {
   const words = [];
   if (startString && startString.length > 0) {
     set.forEach((value) => {
-      if (value.startsWith(startString)) {
+      if (typeof value === 'string' && value.startsWith(startString)) {
         words.push(value.substring(startString.length));
       }
     });
