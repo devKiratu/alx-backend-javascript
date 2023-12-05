b7f7d4ec2b3da450a6897408e3a0e518c6813055
Christopher Ndung'u <dev.kiratu@gmail.com>
2023-12-05 09:58:38 +0300

    validate startString before data manipulation

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,9 +1,11 @@
 export default function cleanSet(set, startString) {
   const words = [];
-  set.forEach((value) => {
-    if (value.startsWith(startString)) {
-      words.push(value.substring(startString.length));
-    }
-  });
+  if (startString && startString.length > 0) {
+    set.forEach((value) => {
+      if (value.startsWith(startString)) {
+        words.push(value.substring(startString.length));
+      }
+    });
+  }
   return words.join('-');
 }
