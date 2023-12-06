fd99175d3222334cb69d19c7acd91eb65ee14e94
Christopher Ndung'u <dev.kiratu@gmail.com>
2023-12-06 14:46:31 +0300

    add solutions to task 2

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -1,8 +1,12 @@
-export interface Teacher {
+interface Teacher {
   firstName: string;
   lastName: string;
   fullTimeEmployee: boolean;
   yearsOfExperience?: number;
   location: string;
   [key: string]: any;
+}
+
+interface Directors extends Teacher {
+  numberOfReports: number;
 }
