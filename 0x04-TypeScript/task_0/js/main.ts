ce925048ef32baf738d060c75e0e8ef4ec80b2b0
Christopher Ndung'u <dev.kiratu@gmail.com>
2023-12-06 13:37:50 +0300

    add solution to task 0

---
+++ 0x04-TypeScript/task_0/js/main.ts
@@ -0,0 +1,50 @@
+export interface Student {
+  firstName: string;
+  lastName: string;
+  age: number;
+  location: string;
+};
+
+const student1: Student = {
+  firstName: 'Peter',
+  lastName: 'Pun',
+  age: 25, 
+  location: 'Nairobi',
+}
+
+const student2: Student = {
+  firstName: 'James',
+  lastName: 'Bond',
+  age: 33,
+  location: 'Kisumu',
+};
+
+const studentsList: Student[] = [student1, student2];
+
+// create table and append students data
+const body: HTMLElement = document.querySelector('body');
+
+const studentsTable: HTMLTableElement = document.createElement('table');
+
+// table headers
+const header = studentsTable.createTHead();
+const headerRow = header.insertRow();
+const nameTh: HTMLTableHeaderCellElement = document.createElement('th');
+const locationTh: HTMLTableHeaderCellElement = document.createElement('th');
+nameTh.innerHTML = 'FirstName';
+locationTh.innerHTML = 'Location';
+
+headerRow.appendChild(nameTh);
+headerRow.appendChild(locationTh);
+
+const tbody = studentsTable.createTBody();
+
+studentsList.forEach((student) => {
+  const row = tbody.insertRow();
+  const name = row.insertCell(0);
+  const location = row.insertCell(1);
+  name.innerHTML = student.firstName;
+  location.innerHTML = student.location;
+});
+
+body.appendChild(studentsTable);
