9f93541ef5b4aa0add305c393d0f520722ada1a7
Christopher Ndung'u <dev.kiratu@gmail.com>
2023-12-06 15:07:15 +0300

    add solutions to task 3

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -10,3 +10,11 @@ interface Teacher {
 interface Directors extends Teacher {
   numberOfReports: number;
 }
+
+interface printTeacherFunction {
+  printTeacher: (firstName: string, lastName: string) => string;
+}
+
+const printTeacher: printTeacherFunction['printTeacher'] = (firstName, lastName) => {
+  return `${firstName[0]}. ${lastName}`;
+}
