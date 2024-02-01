7122f3479cf6475b489df0d6688b37f911ef046c
Christopher Ndung'u <dev.kiratu@gmail.com>
2024-02-01 22:12:39 +0300

    update spies

--- 0x06-unittests_in_js/3-payment.test.js
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -4,21 +4,32 @@ const Utils = require('./utils');
 const expect = require('chai').expect;
 
 describe('sendPaymentRequestToApi', () => {
-  const sandbox = sinon.createSandbox();
+  let calculateNumberSpy;
+  let consoleSpy;
 
   beforeEach(() => {
-    sandbox.spy(Utils, 'calculateNumber');
+    calculateNumberSpy = sinon.spy(Utils, 'calculateNumber');
+    consoleSpy = sinon.spy(console, 'log');
   });
 
   afterEach(() => {
-    sandbox.restore();
+    calculateNumberSpy.restore();
+    consoleSpy.restore();
   });
 
   describe('sendPaymentRequestToApi(100, 20)', () => {
     it('should call Utils.calculateNumber with type == "SUM", a == 100, b == 20', () => {
       sendPaymentRequestToApi(100, 20);
-      expect(Utils.calculateNumber.calledOnce).to.be.true;
-      expect(Utils.calculateNumber.calledWithExactly('SUM', 100, 20)).to.be.true;
+      expect(calculateNumberSpy.calledOnce).to.be.true;
+      expect(calculateNumberSpy.calledWithExactly('SUM', 100, 20)).to.be.true;
+    });
+  });
+
+  describe('verify logging', () => {
+    it('should log The total is: 120', () => {
+      sendPaymentRequestToApi(100, 20);
+      expect(consoleSpy.calledOnce).to.be.true;
+      expect(consoleSpy.calledWithExactly('The total is: 120')).to.be.true;
     });
   });
 });
